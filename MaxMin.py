dc17ae7601a16caa4d90540446606a6dca0b9cb1
nalla <nallagarishashanka@gmail.com>
2024-03-09 20:33:15 +0530

    my third commit

---
+++ MaxMin.py
@@ -0,0 +1,11 @@
+#Using Sort() Method
+a=[20,50,67,98,10,35]
+print(a)
+a.sort()
+print(a)
+print("Largest number in the list is:",a[-1])
+print("Smallest number in the list is:",a[0])
+
+#Using MAX() MIN() METHOD
+print("maximum number in the list is:",max(a))
+print("minimum number in the list is:",min(a))
